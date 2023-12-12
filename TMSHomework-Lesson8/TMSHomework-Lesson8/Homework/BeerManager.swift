60e1bbf119288504ae2653c00de3046b9a63c925
Natallia Mazur <natashamazur@bk.ru>
2023-12-12 23:17:37 +0300

    Created homework project, added SnapKit and two files in Homework folder

---
+++ TMSHomework-Lesson8/TMSHomework-Lesson8/Homework/BeerManager.swift
@@ -0,0 +1,23 @@
+//
+//  BeerManager.swift
+//  Lesson8
+//
+//  Created by berdy on 12.12.23.
+//
+
+import Foundation
+
+class BeerManager {
+    static let shared = BeerManager()
+    private var revenue: Double = 0 // Выручка
+    
+    private init() {}
+    
+    //Метод продажи пива
+    
+    //Метод проверки выручки за сегодня
+    
+    //Метод обнуления выручки после наступления следующего дня
+    
+    //Метод который проверяет и показывает остаток пива
+}
